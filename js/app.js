67ee06d7d570edeff59399020cf01c5be107c1f2
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-26 08:30:23 +0600

    ad secarch bar

--- js/app.js
+++ js/app.js
@@ -1,14 +1,42 @@
-const loadData = async () => {
-    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+const loadData = async (searchText) => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     const res = await fetch(url);
     const data = await res.json();
     displayData(data.data)
 }
 
 const displayData = phones => {
-    const phoneContainer = document.getElementById('phone-container');
+    const phoneContainer = document.getElementById('container');
+    phoneContainer.innerHTML = '';
     phones.forEach(phone => {
-        console.log(phone)
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('card', 'w-full', 'bg-base-100', 'shadow-xl', 'border');
+        phoneDiv.innerHTML = `
+            <figure class="px-10 pt-10">
+                <img src="${phone.image}"
+                    alt="Shoes"
+                    class="rounded-xl" />
+            </figure>
+            <div
+                class="card-body items-center text-center">
+                <h2 class="card-title">${phone.phone_name}</h2>
+                <p>If a dog chews shoes
+                    whose shoes does he
+                    choose?</p>
+                <div class="card-actions">
+                    <button
+                        class="btn btn-primary">Buy
+                        Now</button>
+                </div>
+            </div>
+        `;
+        phoneContainer.appendChild(phoneDiv)
+        // console.log(phone)
     });
 }
+
+document.getElementById('search-button').addEventListener('click', function () {
+    const searchText = document.getElementById('input-filed').value;
+    loadData(searchText)
+})
 loadData()
